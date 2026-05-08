5cc49640350989638e1b0a279dbba176a1e29d95
Marcus <marcus.pedro@aluno.senai.br>
2026-05-08 10:22:05 -0300

    a

---
+++ WebApplication1/Controllers/AgenciaController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AgenciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Codigo()
+        {
+            return View();
+        }
+
+        public IActionResult Descricao()
+        {
+            return View();
+        }
+        //sla
+        public IActionResult Cidade()
+        {
+            return View();
+        }
+
+        public IActionResult Estado()
+        {
+            return View();
+        }
+    }
+}
