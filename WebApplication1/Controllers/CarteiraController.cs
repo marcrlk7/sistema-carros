b13ef0691ec9c76e3d1e2531c08f8c55818b8f06
Marcus <marcus.pedro@aluno.senai.br>
2026-05-08 10:33:22 -0300

    aaaaa

---
+++ WebApplication1/Controllers/CarteiraController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CarteiraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Codigo()
+        {
+            return View();
+        }
+
+        public IActionResult Nome()
+        {
+            return View();
+        }
+
+        public IActionResult Ativa()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Controllers/FuncionarioController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Codigo()
+        {
+            return View();
+        }
+
+        public IActionResult Nome()
+        {
+            return View();
+        }
+
+        public IActionResult Sexo()
+        {
+            return View();
+        }
+
+        public IActionResult Cidade()
+        {
+            return View();
+        }
+
+        public IActionResult Estado()
+        {
+            return View();
+        }
+
+        public IActionResult Funcao()
+        {
+            return View();
+        }
+    }
+}
